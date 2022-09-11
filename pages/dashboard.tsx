2aecc9183b8b886052ae59dd9bd563e9907260c3
daniel-black <drblack651@gmail.com>
2022-09-11 16:52:13 -0500

    sign up, sign in, log out all working

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,27 @@
-import { NextPage } from 'next';
+import type { NextPage } from 'next';
+import { useSession, signOut } from 'next-auth/react';
+
+import { requireAuth } from '../utils/requireAuth';
+
+export const getServerSideProps = requireAuth(async (ctx) => {
+  return { props: {} };
+})
 
 const Dashboard: NextPage = () => {
+  const { data } = useSession();
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      Dashboard
+      <p>{JSON.stringify(data, null, 2)}</p>
+      <button 
+        onClick={() => signOut({ callbackUrl: '/' })}
+        className='bg-rose-300 rounded p-3'
+      >
+        Sign out
+      </button>
+    </div>
+  );
 }
 
 export default Dashboard;
