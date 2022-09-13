7ca74e9776ac61808b13efd5d9c11942a048a802
daniel-black <drblack651@gmail.com>
2022-09-12 20:44:37 -0500

    Pretty broken delete functionality

--- components/TeamPokemon.tsx
+++ components/TeamPokemon.tsx
@@ -4,10 +4,11 @@ import PokemonImage from "./PokemonImage";
 type Props = {
   pokemon: Pokemon | null,
   order: number,
+  deletePokemon: () => void;
 };
 
 const TeamPokemon = (props: Props): JSX.Element => {
-  const { pokemon, order } = props;
+  const { pokemon, order, deletePokemon } = props;
 
   const renderContent = (): JSX.Element => {
     if (!pokemon) {
@@ -29,9 +30,20 @@ const TeamPokemon = (props: Props): JSX.Element => {
     );
   }
 
+  const renderDeleteButton = (): JSX.Element | null => {
+    if (!pokemon) return null;
+    return (
+      <button 
+        onClick={deletePokemon}
+        className="z-10 absolute top-1 right-2 text-rose-100 w-5 h-5 rounded-full bg-rose-400 opacity-0 font-thin group-hover:opacity-100 flex items-center justify-center duration-75 shadow"
+      >X</button>
+    );
+  }
+
   return (
     <div className='flex justify-center items-center h-40 w-40 rounded-xl bg-emerald-100 p-1 relative group'>
       <span className="absolute top-1 left-2 text-emerald-400 font-semibold">{order}</span>
+      {renderDeleteButton()}
       {renderContent()}
     </div>
   );
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -14,7 +14,6 @@ export const getServerSideProps = requireAuth(async (ctx) => {
 
 const Dashboard: NextPage = () => {
   const [searchResult, setSearchResult] = useState<null | Pokemon>(null);
-  // const [team, setTeam] = useState<Array<Pokemon>>([]);
 
   const [p1, setP1] = useState<Pokemon | null>(null);
   const [p2, setP2] = useState<Pokemon | null>(null);
@@ -22,6 +21,13 @@ const Dashboard: NextPage = () => {
   const [p4, setP4] = useState<Pokemon | null>(null);
   const [p5, setP5] = useState<Pokemon | null>(null);
   const [p6, setP6] = useState<Pokemon | null>(null);
+
+  console.log(`p1: ${p1}`);
+  console.log(`p2: ${p2}`);
+  console.log(`p3: ${p3}`);
+  console.log(`p4: ${p4}`);
+  console.log(`p5: ${p5}`);
+  console.log(`p6: ${p6}`);
 
   const [teamSize, setTeamSize] = useState(0);
 
@@ -59,7 +65,7 @@ const Dashboard: NextPage = () => {
 
       {searchResult && (
         <button 
-          className='bg-sky-400 text-sky-900 rounded-full p-2 w-64 border text-xl border-sky-600 hover:bg-sky-500'
+          className='bg-sky-400 text-sky-900 rounded-full p-2 w-72 border text-xl border-sky-600 hover:bg-sky-500'
           onClick={addPokemonToTeam}
         >
           Add <span className='capitalize'>{searchResult?.name}</span> to team!
@@ -68,12 +74,12 @@ const Dashboard: NextPage = () => {
 
 
       <div className='grid grid-cols-3 max-w-fit mb-20 gap-0.5'>
-        <TeamPokemon pokemon={p1} order={1} />
-        <TeamPokemon pokemon={p2} order={2} />
-        <TeamPokemon pokemon={p3} order={3} />
-        <TeamPokemon pokemon={p4} order={4} />
-        <TeamPokemon pokemon={p5} order={5} />
-        <TeamPokemon pokemon={p6} order={6} />
+        <TeamPokemon pokemon={p1} order={1} deletePokemon={() => setP1(null)} />
+        <TeamPokemon pokemon={p2} order={2} deletePokemon={() => setP2(null)} />
+        <TeamPokemon pokemon={p3} order={3} deletePokemon={() => setP3(null)} />
+        <TeamPokemon pokemon={p4} order={4} deletePokemon={() => setP4(null)} />
+        <TeamPokemon pokemon={p5} order={5} deletePokemon={() => setP5(null)} />
+        <TeamPokemon pokemon={p6} order={6} deletePokemon={() => setP6(null)} />
       </div>
 
     </div>
